733a16eaec4665cf05b58852272e384bd3454637
Victor Levasseur <victorlevasseur52@gmail.com>
2015-11-18 16:55:09 +0100

    Ajout d'une collection de MainJoueur à Manche

--- JeuDuMenhir/src/fr/utt/girardguittard/levasseur/menhir/Manche.java
+++ JeuDuMenhir/src/fr/utt/girardguittard/levasseur/menhir/Manche.java
@@ -1,5 +1,11 @@
 package fr.utt.girardguittard.levasseur.menhir;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import fr.utt.girardguittard.levasseur.menhir.joueurs.Joueur;
+import fr.utt.girardguittard.levasseur.menhir.joueurs.MainJoueur;
+
 /**
  * La classe Manche représente une manche et permet de faire son déroulement.
  */
@@ -16,12 +22,24 @@ public class Manche {
 	private int joueurTour;
 	
 	/**
+	 * Liste des mains des joueurs
+	 */
+	private ArrayList<MainJoueur> mainsDesJoueurs;
+	
+	/**
 	 * Constructeur de Manche.
 	 * Construit une manche vide.
 	 */
-	public Manche() {
+	public Manche(ArrayList<Joueur> joueurs) {
 		this.tourActuel = Saison.PRINTEMPS;
 		this.joueurTour = 0;
+		this.mainsDesJoueurs = new ArrayList<MainJoueur>();
+		
+		for(int i = 0; i < joueurs.size(); i++) {
+			MainJoueur nouvelleMain = new MainJoueur(joueurs.get(i), this);
+			//joueurs.get(i).setMain(nouvelleMain);
+			this.mainsDesJoueurs.add(nouvelleMain);
+		}
 	}
 	
 	/**
@@ -31,4 +49,12 @@ public class Manche {
 		
 	}
 	
+	/**
+	 * Retourne un joueur.
+	 * @param numero le numéro du joueur
+	 * @return 
+	 */
+	public Joueur getJoueur(int numero) {
+		return this.mainsDesJoueurs.get(numero).getJoueur();
+	}
 }
