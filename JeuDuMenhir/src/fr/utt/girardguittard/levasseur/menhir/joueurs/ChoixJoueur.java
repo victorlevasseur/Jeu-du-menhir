a05bb2c85e933f4790d06b63682f0e1213609284
ElDawgerino <bouhahah@gmail.com>
2015-11-18 18:15:38 +0100

    Création de la classe ChoixJoueur permettant de stocker les retours de deciderChoixDuTour

---
+++ JeuDuMenhir/src/fr/utt/girardguittard/levasseur/menhir/joueurs/ChoixJoueur.java
@@ -0,0 +1,63 @@
+package fr.utt.girardguittard.levasseur.menhir.joueurs;
+
+import fr.utt.girardguittard.levasseur.menhir.cartes.CarteIngredient;
+import fr.utt.girardguittard.levasseur.menhir.cartes.Action;
+
+/**
+ * Classe contenant la carte choisie par le joueur, l'action choisie et sa cible 
+ * Cette carte sert de retour à la méthode deciderChoixDuTour
+ *
+ */
+public class ChoixJoueur {
+	
+	/**
+	 * La carte ingrédient choisie par le joueur
+	 */
+	private final CarteIngredient carteChoisie;
+	
+	/**
+	 * Le numéro correspondant au joueur ciblé
+	 */
+	private final int cible;
+	
+	/**
+	 * L'action choisie par le joueur
+	 */
+	private final Action actionChoisie;
+	
+	/**
+	 * Le constructeur de la classe, permettant d'initialiser les variables
+	 * @param carteChoisie la carte choisie par le joueur
+	 * @param cible le joueur ciblé
+	 * @param actionChoisie l'action choisie par le joueur
+	 */
+	public ChoixJoueur(CarteIngredient carteChoisie, int cible, Action actionChoisie) {
+		this.carteChoisie = carteChoisie;
+		this.cible = cible;
+		this.actionChoisie = actionChoisie;
+	}
+
+	/**
+	 * Getter de la carte choisie
+	 * @return la carte choisie par le joueur
+	 */
+	public CarteIngredient getCarteChoisie() {
+		return this.carteChoisie;
+	}
+	
+	/**
+	 * Getter du numéro du joueur cible
+	 * @return le numéro du joueur ciblé
+	 */
+	public int getCible() {
+		return this.cible;
+	}
+	
+	/**
+	 * Getter de l'action
+	 * @return l'action choisie par le joueur
+	 */
+	public Action getActionChoisie() {
+		return this.actionChoisie;
+	}
+}
