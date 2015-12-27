7ff907af759144af94ea36e31f9c9998e5c673e3
ElDawgerino <bouhahah@gmail.com>
2015-12-27 22:03:49 +0100

    Initialisation de ViewMenuPrincipal

---
+++ JeuDuMenhir/src/fr/utt/girardguittard/levasseur/menhir/ui/gui/ViewMenuPrincipal.java
@@ -0,0 +1,42 @@
+package fr.utt.girardguittard.levasseur.menhir.ui.gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class ViewMenuPrincipal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ViewMenuPrincipal frame = new ViewMenuPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ViewMenuPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
