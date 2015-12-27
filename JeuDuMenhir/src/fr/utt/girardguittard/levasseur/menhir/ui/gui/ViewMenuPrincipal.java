12eabf42144ec0de8a2c4bc2be7061768090ec94
ElDawgerino <bouhahah@gmail.com>
2015-12-27 22:17:09 +0100

    Conception de l'interface dans windowbuilder

--- JeuDuMenhir/src/fr/utt/girardguittard/levasseur/menhir/ui/gui/ViewMenuPrincipal.java
+++ JeuDuMenhir/src/fr/utt/girardguittard/levasseur/menhir/ui/gui/ViewMenuPrincipal.java
@@ -6,6 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JSpinner;
+import javax.swing.JLabel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class ViewMenuPrincipal extends JFrame {
 
@@ -32,11 +42,58 @@ public class ViewMenuPrincipal extends JFrame {
 	 */
 	public ViewMenuPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 501, 194);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JRadioButton rdbtnPartieSimple = new JRadioButton("Partie Simple");
+		
+		JRadioButton rdbtnPartieAvance = new JRadioButton("Partie Avancée");
+		
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setModel(new SpinnerNumberModel(1, 1, 5, 1));
+		
+		JLabel lblNombreDeJoueur = new JLabel("Nombre de Joueur");
+		
+		JButton btnJouer = new JButton("Jouer");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(50)
+					.addComponent(rdbtnPartieSimple)
+					.addPreferredGap(ComponentPlacement.RELATED, 158, Short.MAX_VALUE)
+					.addComponent(rdbtnPartieAvance)
+					.addGap(47))
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addContainerGap(121, Short.MAX_VALUE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addComponent(btnJouer, GroupLayout.PREFERRED_SIZE, 106, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED))
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addComponent(lblNombreDeJoueur)
+							.addGap(59)))
+					.addComponent(spinner, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(154))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(rdbtnPartieSimple)
+						.addComponent(rdbtnPartieAvance))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(spinner, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNombreDeJoueur))
+					.addGap(18)
+					.addComponent(btnJouer)
+					.addContainerGap(225, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
-
 }
