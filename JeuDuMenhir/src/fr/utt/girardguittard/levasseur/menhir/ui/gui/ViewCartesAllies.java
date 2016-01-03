12e4823585ee2cea1d85498a9d2f147fd32a7594
ElDawgerino <bouhahah@gmail.com>
2016-01-03 15:47:52 +0100

    Ajout d'un boutton permettant de choisir la carte allié

--- JeuDuMenhir/src/fr/utt/girardguittard/levasseur/menhir/ui/gui/ViewCartesAllies.java
+++ JeuDuMenhir/src/fr/utt/girardguittard/levasseur/menhir/ui/gui/ViewCartesAllies.java
@@ -5,6 +5,8 @@ import java.util.Observable;
 import java.util.Observer;
 import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -19,11 +21,21 @@ public class ViewCartesAllies extends JPanel implements Observer {
 			
 	private JLabel affichageCarte = new JLabel();
 	
+	private JButton bouttonJouer = new JButton("Jouer");
+	
+	private ControllerCartesAllies controller;
+	
+	private JComboBox comboCible;
+	
 	public ViewCartesAllies(MainJoueur m) {
 		//Ajout en tant qu'observateur
 		this.main = m;
 		main.addObserver(this);
-				
+
+		//Ajout du controller au boutton
+		controller = new ControllerCartesAllies(main, this);
+		bouttonJouer.addActionListener(controller);
+		
 		//On affiche les données de la carte
 		if(main.getCarteAllies() != null) {
 			affichageCarte.setText("<html><pre>" + main.getCarteAllies().toString() + "</pre></html>");
@@ -35,16 +47,19 @@ public class ViewCartesAllies extends JPanel implements Observer {
 		
 		//Ajout des éléments au panel
 		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		this.add(affichageCarte);	
+		this.add(affichageCarte);
+		this.add(bouttonJouer);
 	}
 
 	public void update(Observable obs, Object obj) {
 		if(obs == main){
 			if(main.getCarteAllies() != null) {
 				affichageCarte.setText("<html><pre>" + main.getCarteAllies().toString() + "</pre></html>");
+				bouttonJouer.setEnabled(true);
 			}
 			else {
 				affichageCarte.setText("Vous ne disposez pas de carte alliés.");
+				bouttonJouer.setEnabled(false);
 			}
 		}
 	}
