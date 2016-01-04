8eb7ea51e8b9b7d9919fe7a315a86ccfdc7188e7
Victor Levasseur <victorlevasseur52@gmail.com>
2016-01-05 00:13:27 +0100

    Ajout de l'en-tête de licence GNU GPLv2 à tous les fichiers .java

--- JeuDuMenhir/src/fr/utt/girardguittard/levasseur/menhir/cartes/package-info.java
+++ JeuDuMenhir/src/fr/utt/girardguittard/levasseur/menhir/cartes/package-info.java
@@ -1,3 +1,23 @@
+/*
+	JeuDuMenhir is a board game adapted into a computer game.
+	Copyright (C) 2015-2016  
+	Antoine Girard Guittard (antoine.girard_guittard@utt.fr), Victor Levasseur (victorlevasseur52@gmail.com)
+	
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 /**
  * Contient les outils liés aux cartes, c'est-à-dire les cartes ingrédients et alliés, la fabrique des cartes,
  * la classe permettant de gérer un tas de cartes et des classes permettant de décrire l'action d'une carte
